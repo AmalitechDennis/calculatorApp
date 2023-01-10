e834df5b3a38cc0f7740ff587b89cbf180f77ae8
AmalitechDennis <dennis.tweneboah@amalitech.org>
2023-01-10 23:44:21 +0000

    js update

--- calculator.js
+++ calculator.js
@@ -42,9 +42,6 @@ function insertComma(num){
         str[0] = str[0].replaceAll(',','');
         str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
     }
-    if (str[1] && str[1].length >= 4) {
-        str[1] = str[1].replace(/(\d{3})/g, '$1,');
-    }
     return str.join('.');
 }
 //clears screen area
